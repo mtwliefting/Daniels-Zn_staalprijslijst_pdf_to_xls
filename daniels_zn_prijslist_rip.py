b0ed1aa65a0d6320061d23699f998f18e1e74827
mtwliefting <mander@snodevormgevers.nl>
2020-02-19 20:38:59 +0100

    Adding Total Sum and Multiplexer to the Excel file

--- daniels_zn_prijslist_rip.py
+++ daniels_zn_prijslist_rip.py
@@ -5,39 +5,43 @@ import os
 
 workbook = Workbook()
 sheet = workbook.active
-name = ["A","B"]
+sheet.column_dimensions["A"].width = 30
+
+name = ["A","B","C","D"]
 a = 0
-
-
 file = "https://www.danielsmetalen.nl/prijslijst_constructiestalen_plaatmaterialen.pdf"
-
 data = read_pdf(wget.download(file), output_format="json")
 
 for i in data[0]["data"]:
         #filename = i['actual']['stationmeasurements']
         a = a + 1
-        print()
         p = i[0]["text"]
         item = p.split("€ ")
         
         col_A = name[0] + str(a)
         col_B = name[1] + str(a)
-        
-        print(col_A)
+        col_C = name[2] + str(a)
+        col_D = name[3] + str(a)
         
         if item[-1].replace(",", "").isdigit():
-                print(item[0] + " € " + item[-1])
                 
                 sheet[col_A] = item[0] #data tot excel
-                sheet[col_B] = item[-1] #data tot excel
+                sheet[col_B].number_format = '#,##0.00€' 
+                sheet[col_B] = float(item[-1].replace(',', '.'))
+                sheet[col_C] = 0
+                sheet[col_D].number_format = '#,##0.00€' 
+                sheet[col_D] = "=SUM(" + col_B + "*" + col_C + " )"         
+                
         else:
-                print(item[0])
-                
                 sheet[col_A] = item[0] #data tot excel
+
+# Get total prices and numbers
+sheet[name[3] + str(a+1)].number_format = '#,##0.00€' 
+sheet[name[3] + str(a+1)] = "=SUM(D4:D" + str(a) + ")"
+sheet[name[2] + str(a+1)] = "=SUM(C4:C" + str(a) + ")"
+sheet[name[0] + str(a+1)] = "Totaal:"
         
         
-        
-     
 x = file.split("/")
 os.remove(x[3])
 
